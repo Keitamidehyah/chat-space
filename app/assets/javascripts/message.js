93179c5898e97e6a0c096dab009c11b7d2dc494e
Keitamidehyah <56815486+Keitamidehyah@users.noreply.github.com>
2019-11-09 17:31:48 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -12,28 +12,35 @@ $(function(){
                 </p>`
     return html;
   }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
 
+  })
+    var reloadMessages = function () {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+        var last_message_id = $('.message:last').data("message-id");
+    
     $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
+      url: "api/messages",
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+       data: {last_id: last_message_id}
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.comments').append(html);
-      $('.chat').animate({scrollTop: $('.chat')[0].scrollHeight});
-      $('form').get(0).reset();
-      $('.form__submit').removeAttr('disabled')
+    .done(function(messages) {
+      var inserthtml = '';
+      messages.forEach(function (message){
+        insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML);
+      })
+      $('messages').animate({scrollTop: $('messages')[0].scrollHeight}, 'fast');
     })
     .fail (function(){
       alert('メッセージ送信に失敗しました');
-    })
-   })
-  })
+    });
+  }
+ };
+setInterval(reloadMessages, 5000);
+ });
